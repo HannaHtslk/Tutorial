c30872c55437fc65c3c494ae2ba0311f92958001
AnnaHtslk <hannah.htslk@gmail.com>
2024-03-13 17:27:44 +0200

    stopped on arrays

---
+++ JS/arrays.tasks.js
@@ -0,0 +1,80 @@
+/*
+? Напиши скрипт для обчислення площі прямокутника зі сторонами,
+? значення яких зберігаються у змінній values у вигляді рядка.
+? Значення гарантовано розділені пробілом.
+ */
+
+//    const values = '8 11';
+// function getArea(values) {
+ 
+//     const valuesArr = values.split(' ');
+//     const totalArea = Number(valuesArr[0]) * Number(valuesArr[1]);
+//     return totalArea;
+// }
+
+// console.log(getArea(values));
+
+
+/*
+? Напиши скрипт, який підраховує суму всіх парних чисел у масиві.
+*/
+// const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 14];
+// let total = 0;
+// function getTotal(numbers) {
+//     // for (const number of numbers) {
+//     //     if (number % 2 === 0) {
+//     //         total += number;
+//     //     }
+      
+//     // }
+//     // return total;
+
+
+
+//     for (let i = 0; i < numbers.length; i++) {
+//         if (numbers[i] % 2 === 0) {
+//             total += numbers[i];
+//             }
+//     }
+//     return total;
+// }
+// console.log(getTotal(numbers));
+
+
+/*
+? Напиши скрипт, який виводить у консоль ім'я та телефонний номер користувача.
+? У змінних names та phones зберігаються рядки імен та телефонних номерів,
+? розділені комами. Порядковий номер імен та телефонів у рядках вказують на
+? відповідність. Кількість імен та телефонів гарантовано однакова.
+*/
+
+// const names = 'Jacob,William,Solomon,Artemis';
+// const phones = '89001234567,89001112233,890055566377,890055566300';
+
+// const namesArray = names.split(',');
+// const phonesArray = phones.split(',');
+// for (let i = 0; i < namesArray.length; i++) {
+//     console.log(`${namesArray[i]} - ${phonesArray[i]}`);
+// }
+ 
+/*
+? Напиши скрипт, який виводить у консоль усі слова рядка
+? крім першого і останнього. Результуючий рядок не повинен починатися
+? або закінчуватися символ пробілу.
+? Скрипт повинен працювати для будь-якого рядка.
+*/
+// const string = 'Welcome to the future';
+
+/*
+? Напиши скрипт, який «розгортає» рядок (зворотний порядок букв)
+? та виводить її в консоль.
+*/
+const string = 'Welcome to the future';
+
+/*
+? Напиши скрипт пошуку найменшого числа у масиві. Код повинен працювати
+? для будь-якого масиву чисел. Використовуй цикл для розв'язання задачі.
+*/
+
+// const values = [17, -10, 94, -30, 1, 23, -20];
+
