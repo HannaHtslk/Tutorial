d4e6ed77d852ce8fbe6808c4c17a97191b0e35b2
AnnaHtslk <hannah.htslk@gmail.com>
2024-03-20 14:24:31 +0200

    first task

---
+++ JS/module5.js
@@ -0,0 +1,17 @@
+function getTotalSalary(array) {
+    let salary = 0;
+
+    for (const key of array) {
+        salary += key;
+    }
+    return salary;
+}
+
+let
+
+
+
+
+function showMessage() {
+    
+}
