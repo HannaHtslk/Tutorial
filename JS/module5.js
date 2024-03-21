d0fa15c3b58cc7d32f9a94cf39ba5b8187df51f5
AnnaHtslk <hannah.htslk@gmail.com>
2024-03-21 21:31:14 +0200

    upd

--- JS/module5.js
+++ JS/module5.js
@@ -131,63 +131,101 @@
 // ])
 // ); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
 
-const getUsersWithFriend = (users, friendName) => users.filter(user => user.friends.includes(friendName));
+// const getUsersWithFriend = (users, friendName) => users.filter(user => user.friends.includes(friendName))
 
 
-const allUsers = [
-  {
-    name: "Moore Hensley",
-    friends: ["Sharron Pace"]
-  },
-  {
-    name: "Sharlene Bush",
-    friends: ["Briana Decker", "Sharron Pace"]
-  },
-  {
-    name: "Ross Vazquez",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
-  },
-  {
-    name: "Elma Head",
-    friends: ["Goldie Gentry", "Aisha Tran"]
-  },
-  {
-    name: "Carey Barr",
-    friends: ["Jordan Sampson", "Eddie Strong"]
-  },
-  {
-    name: "Blackburn Dotson",
-    friends: ["Jacklyn Lucas", "Linda Chapman"]
-  },
-  {
-    name: "Sheree Anthony",
-    friends: ["Goldie Gentry", "Briana Decker"]
-  }
-];
-
-console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
-// [
+// const allUsers = [
+//   {
+//     name: "Moore Hensley",
+//     friends: ["Sharron Pace"]
+//   },
 //   {
 //     name: "Sharlene Bush",
 //     friends: ["Briana Decker", "Sharron Pace"]
 //   },
 //   {
-//     name: "Sheree Anthony",
-//     friends: ["Goldie Gentry", "Briana Decker"]
-//   }
-// ]
-
-console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
-// [
+//     name: "Ross Vazquez",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
+//   },
 //   {
 //     name: "Elma Head",
 //     friends: ["Goldie Gentry", "Aisha Tran"]
 //   },
 //   {
+//     name: "Carey Barr",
+//     friends: ["Jordan Sampson", "Eddie Strong"]
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"]
+//   },
+//   {
 //     name: "Sheree Anthony",
 //     friends: ["Goldie Gentry", "Briana Decker"]
 //   }
-// ]
+// ];
 
-console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
+// console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
+// // [
+// //   {
+// //     name: "Sharlene Bush",
+// //     friends: ["Briana Decker", "Sharron Pace"]
+// //   },
+// //   {
+// //     name: "Sheree Anthony",
+// //     friends: ["Goldie Gentry", "Briana Decker"]
+// //   }
+// // ]
+
+// console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
+// // [
+// //   {
+// //     name: "Elma Head",
+// //     friends: ["Goldie Gentry", "Aisha Tran"]
+// //   },
+// //   {
+// //     name: "Sheree Anthony",
+// //     friends: ["Goldie Gentry", "Briana Decker"]
+// //   }
+// // ]
+
+// console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
+
+
+// const students = [
+//   { name: "Mango", score: 83 },
+//   { name: "Poly", score: 59 },
+//   { name: "Ajax", score: 37 },
+//   { name: "Kiwi", score: 94 },
+//   { name: "Houston", score: 64 },
+// ];
+
+// const namesArray = students.map(student => student.name);
+// console.log(namesArray);
+
+
+// const averageScore = students.reduce((totalScore, currentScore) => {
+//   totalScore += currentScore.score;
+//   return totalScore;
+// }, 0) / students.length;
+// console.log(averageScore)
+
+// const lowScore = students.filter(student => student.score < 85).toSorted((a, b) => a.score - b.score);
+// console.log(lowScore);
+
+// const totalHighScore = students.filter(student => student.score < 85).reduce((total, currentScore) => {
+//   total += currentScore.score;
+//   return total;
+// }, 0)
+// console.log(totalHighScore);
+
+// const checkScore = students.every(student => student.score > 90);
+// console.log(checkScore);
+
+// const checkMinScore = students.some(student => student.score > 90);
+// console.log(checkMinScore);
+
+
+
+
 
---
+++ JS/module6.js
@@ -0,0 +1,26 @@
+// const library = {
+// 	books: 1923,
+// 	logBookCount() {
+// 		console.log(this.books);
+// 	}
+// };
+
+// const showBooks = library.logBookCount.bind(library);
+// console.log(showBooks())
+
+
+const hotel = {
+  username: "Resort hotel",
+  showThis() {
+    const foo = () => {
+      console.log("this in foo: ", this);
+    };
+
+    foo();
+    console.log("this in showThis: ", this);
+  },
+};
+
+hotel.showThis();
+// this in foo: {username: 'Resort hotel', showThis: ƒ}
+// this in showThis: {username: 'Resort hotel',showThis: ƒ}
