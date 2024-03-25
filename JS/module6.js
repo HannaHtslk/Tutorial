c70eafe7a5aa87532f5cc3388ef9217ba0c62c1e
AnnaHtslk <hannah.htslk@gmail.com>
2024-03-25 14:27:41 +0200

    upd

--- JS/module6.js
+++ JS/module6.js
@@ -27,28 +27,49 @@
 
 
 
-class User {
-  #email;
-  #login;
-  constructor() {
-    this.#email = 'anna@gmail.com',
-      this.#login = 'qwerty123',
-      this.phone = 32445;
-  }
+// class User {
+//   #email;
+//   #login;
+//   constructor() {
+//     this.#email = 'anna@gmail.com',
+//       this.#login = 'qwerty123',
+//       this.phone = 32445;
+//   }
   
-  get email() {
-    return this.#email
-  }
-  set email(newEmail) {
-    this.#email = newEmail;
-  }
-}
-const admin = new User()
-admin.name = 'Tony'
+//   get email() {
+//     return this.#email
+//   }
+//   set email(newEmail) {
+//     this.#email = newEmail;
+//   }
+// }
+// const admin = new User()
+// admin.name = 'Tony'
 
-console.log(admin.email)
-console.log(admin.email = 'netty@gmail.com')
+// console.log(admin.email)
+// console.log(admin.email = 'netty@gmail.com')
 
 
 
 // console.log(admin.name, admin.login);
+
+// Створіть клас Rectangle, що містить властивості width та height. Додайте метод getArea(), який повертає площу прямокутника.
+// 
+
+class Rectangle {
+
+  constructor(width, height) {
+   this.width = width;
+    this.height = height;
+  }
+
+  // getArea() {
+  //   return this.width * this.height;
+  // }
+  get 
+}
+
+
+
+const rectangle1 = new Rectangle(10, 5);
+console.log(rectangle1.getArea()); // 50
