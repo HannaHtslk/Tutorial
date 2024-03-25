f9d8409f31dcf4a8b237a42f178d6f4c77c54e55
AnnaHtslk <hannah.htslk@gmail.com>
2024-03-25 14:05:29 +0200

    upd

--- JS/module6.js
+++ JS/module6.js
@@ -9,18 +9,46 @@
 // console.log(showBooks())
 
 
-const hotel = {
-  username: "Resort hotel",
-  showThis() {
-    const foo = () => {
-      console.log("this in foo: ", this);
-    };
+// const hotel = {
+//   username: "Resort hotel",
+//   showThis() {
+//     const foo = () => {
+//       console.log("this in foo: ", this);
+//     };
 
-    foo();
-    console.log("this in showThis: ", this);
-  },
-};
+//     foo();
+//     console.log("this in showThis: ", this);
+//   },
+// };
 
-hotel.showThis();
+// hotel.showThis();
 // this in foo: {username: 'Resort hotel', showThis: ƒ}
 // this in showThis: {username: 'Resort hotel',showThis: ƒ}
+
+
+
+class User {
+  #email;
+  #login;
+  constructor() {
+    this.#email = 'anna@gmail.com',
+      this.#login = 'qwerty123',
+      this.phone = 32445;
+  }
+  
+  get email() {
+    return this.#email
+  }
+  set email(newEmail) {
+    this.#email = newEmail;
+  }
+}
+const admin = new User()
+admin.name = 'Tony'
+
+console.log(admin.email)
+console.log(admin.email = 'netty@gmail.com')
+
+
+
+// console.log(admin.name, admin.login);
